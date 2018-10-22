eabeda4f7ef00c02af6caeaf6103296b841dd237
stressGC <g.cosson@live.fr>
2018-10-22 19:27:37 -0400

    added possibilities computation

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,52 @@
-coords = "123456789"
+import numpy as np
+
+COORDS = "123456789"
 
 class Sudoku:
 
-    def __init__(self, board):
-        self.generate_coords()
+    """
+    INITIALIZATION 
+    """
+    def __init__(self, grid):
+        self.cells = self.generate_coords()
+        self.possibilities = self.generate_possibilities(grid)
 
 
+    """
+    generates all the coordinates of the cells
+    """
     def generate_coords(self):
+
         all_cells_coords = []
-        for a in coords:
-            for b in coords:
+
+        for a in COORDS:
+            
+            for b in COORDS:
                 new_coords = a + b
                 all_cells_coords.append(new_coords)
         
-        self.cells = all_cells_coords
+        return all_cells_coords
 
-sudoku = Sudoku("aeeee")
+    """
+    generates all possible value remaining for each cell
+    """
+    def generate_possibilities(self, grid):
+
+        grid_as_list = list(grid)
+
+        possibilities = dict()
+        default_possibilities = list(range(1,10))
+
+        for index, coords in enumerate(self.cells):
+
+            # if value is 0, then the cell can have any value in [1, 9]
+            if grid_as_list[index] == "0":
+                possibilities[coords] = default_possibilities
+            # else value is already defined, possibilities is this value
+            else:
+                possibilities[coords] = [int(grid_as_list[index])]
+
+        return possibilities
+
+grid = "000079065000003002005060093340050106000000000608020059950010600700600000820390000"
+sudoku = Sudoku(grid)
